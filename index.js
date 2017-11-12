6b02d83273ca177c0a9c4d636c75ba6f38cba863
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-12 20:10:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function BoardMember(na,hs,tr){
     this.releasePressStatement = function(){
       return "You will see great things from Scuber."
     }
+    this.sayHi = function(){
+      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
+    }
 }
 ///BoardMember.prototype.veto(){
 ///}
